241d057308227d4793be2d4f4a4293d242882a58
Tuan <voanhtuan103@gmail.com>
2024-01-14 02:32:33 +0700

    test

--- js/index.js
+++ js/index.js
@@ -1,15 +1,5 @@
 (function() {
-  const audio = new Audio('Beautiful-In-White-Shayne.mp3');
-
-  // Function to play the audio
-  function playAudio() {
-    alert('abc');
-      audio.play();
-      // You can also add other actions here if needed
-  }
-
-  // Play the audio when the page is fully loaded
-  window.addEventListener('load', playAudio);
+  
   function $(id) {
     return document.getElementById(id);
   }
@@ -20,6 +10,17 @@
       timer = null;
   console.log('wat', card);
   openB.addEventListener('click', function () {
+    const audio = new Audio('Beautiful-In-White-Shayne.mp3');
+
+    // Function to play the audio
+    function playAudio() {
+      alert('abc');
+        audio.play();
+        // You can also add other actions here if needed
+    }
+
+    // Play the audio when the page is fully loaded
+    window.addEventListener('load', playAudio);
     card.setAttribute('class', 'open-half');
     if (timer) clearTimeout(timer);
     timer = setTimeout(function () {
